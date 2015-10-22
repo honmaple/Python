0008a14c3c1f2dc2f82243efd53e2d988f4f5a36
honmaple <xiyang0807@gmail.com>
2015-10-23 04:19:24 +0800

    novel->python3

--- novel.py
+++ novel.py
@@ -7,7 +7,8 @@
 #   Created Time: 2015年10月19日 星期一 00时27分41秒
 #*************************************************************************
 import urllib
-import urllib2
+import urllib.request
+from bs4 import BeautifulSoup
 import re
 import sys
 
@@ -57,10 +58,9 @@ class Novel:
             # url = 'http://202.119.112.75/s/2001/t/2016/p/5/i/' + str(pageIndex) + '/list.htm'
             url = 'http://tieba.baidu.com' + url + '?see_lz=1'
             #构建请求的request
-            request = urllib2.Request(url,headers = self.headers)
+            request = urllib.request.Request(url,headers = self.headers)
             #利用urlopen获取页面代码
-            response = urllib2.urlopen(request)
-            # pattern = re.compile('<div id="p_content.*?">.*?<cc.*?>(.*?)</cc>',re.S)
+            response = urllib.request.urlopen(request)
             #将页面转化为UTF-8编码
             pageCode = response.read().decode('utf-8')
             pattern = re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
@@ -68,26 +68,36 @@ class Novel:
             for item in items:
                 sys.stdout.write(self.tool.replace(item))
                 sys.stdout.flush()
-                print '\n'
+                print('\n')
                # print self.tool.replace(item),
             pattern = re.compile('<div id="post_content.*?>(.*?)</div>',re.S)
             items = re.findall(pattern,pageCode)
             for item in items:
                 sys.stdout.write(self.tool.replace(item))
                 sys.stdout.flush()
-                print ''
+                print('')
                 # print self.tool.replace(item),
                 return
-        except urllib2.URLError, e:
+        except urllib.request.URLError as e:
             if hasattr(e,"reason"):
-                print u"连接baidu失败,错误原因",e.reason
+                print(u"连接baidu失败,错误原因",e.reason)
                 return None
+    def getPage1(self,url):
+        url = 'http://tieba.baidu.com' + url + '?see_lz=1'
+        request = urllib.request.Request(url, headers=self.headers)
+        response = urllib.request.urlopen(url)
+        content = response.read().decode("utf-8")
+        soup = BeautifulSoup(content,"lxml")
+        div_soup = soup.find_all("div",{'class':'novel-post-content'})
+        for td in div_soup:
+            print(td.get_text('\n    ','<br/><br/>'))
+
     def getChioce(self,url):
         #构建请求的request
         url = url
-        request = urllib2.Request(url,headers = self.headers)
+        request = urllib.request.Request(url,headers = self.headers)
         #利用urlopen获取页面代码
-        response = urllib2.urlopen(request)
+        response = urllib.request.urlopen(request)
         pattern = re.compile('<ul id="thread_top_list.*?>(.*?)<i class="icon-top.*?>(.*?)<a href=\"(.*?)\".*?>(.*?)</ul>',re.S)
         # pattern = re.compile('<div class="novel-post-content">(.*?)</div>',re.S)
         #将页面转化为UTF-8编码
@@ -95,33 +105,40 @@ class Novel:
         items = re.findall(pattern,content)
         for item in items:
             # return item[2]
-            self.getPage(item[2]),
+            self.getPage(item[2])
+
     def getChioce1(self,url):
-        #构建请求的request
-        url = url
-        request = urllib2.Request(url,headers = self.headers)
+
+        request = urllib.request.Request(url,headers = self.headers)
         #利用urlopen获取页面代码
-        response = urllib2.urlopen(request)
-        pattern = re.compile('<div class="novel-post-content">.*?</div>.*?<div class="novel-post-bottom.*?>',re.S)
+        response = urllib.request.urlopen(request)
+        pattern = re.compile('<ul id="thread_top_list.*?>(.*?)<i class="icon-top.*?>(.*?)<a href=\"(.*?)\".*?>(.*?)</ul>',re.S)
+        # pattern = re.compile('<div class="novel-post-content">(.*?)</div>',re.S)
         #将页面转化为UTF-8编码
         content = response.read().decode('utf-8')
         items = re.findall(pattern,content)
         for item in items:
             # return item[2]
-            self.getPage(item)
+            self.getPage1(item[2])
 
 
     #开始方法
     def start(self):
-        print u"请输入:"
-        print u"1.完美世界  2.我欲封天   3.戮仙"
-        input = raw_input()
-        if input == '1':
+        print(u"请输入:")
+        print(u"1.完美世界  2.我欲封天 3.天火大道 4.九阳踏天  5.戮仙")
+        input1 = input()
+        if input1 == '1':
             url = 'http://tieba.baidu.com/f?kw=%CD%EA%C3%C0%CA%C0%BD%E7%D0%A1%CB%B5&fr=ala0&loc=rec'
             self.getChioce(url)
-        elif input == '2':
+        elif input1 == '2':
             url = 'http://tieba.baidu.com/f?kw=%CE%D2%D3%FB%B7%E2%CC%EC&fr=ala0&loc=rec'
             self.getChioce(url)
+        elif input1 == '3':
+            url = 'http://tieba.baidu.com/f?kw=%CC%EC%BB%F0%B4%F3%B5%C0&fr=ala0&loc=rec'
+            self.getChioce(url)
+        elif input1 == '4':
+            url = 'http://tieba.baidu.com/f?kw=%BE%C5%D1%F4%CC%A4%CC%EC&fr=ala0&loc=rec'
+            self.getChioce1(url)
         else:
             url = 'http://tieba.baidu.com/f?kw=%C2%BE%CF%C9&fr=ala0&loc=rec'
             self.getChioce1(url)
